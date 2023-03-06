f2968ee59506bc2773bcea991082307246b0103f
olaide <olaideoluwatobiloba1@gmail.com>
2023-03-06 14:22:15 +0100

    a new portfolio website i created

---
+++ index.js
@@ -0,0 +1,29 @@
+gsap.from('.box', { duration: 2, y: '-100%', ease: 'bounce' })
+// gsap.from('.skills', { duration: 3, opacity: 0, delay: 1, stagger: .5 })
+gsap.fromTo('.home-text h1', {opacity: 0, scale: 0, rotation: 720}, {duration: 1, delay: 1 , opacity: 1, scale: 1, rotation: 0 })
+gsap.from('.about-part h3 span', { duration: 3, opacity: 0, delay: 1, stagger: .5 })
+
+
+
+
+const header = document.querySelector('header');
+window.addEventListener ('scroll', function() {
+    header.classList.toggle ('sticky', window.scrollY > 100)
+});
+
+let menu = document.querySelector('#menu-list');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('open');
+};
+
+
+ 
+
