fb1e005d5d6a608b03e0bb581893c8acdc45246f
olaide <olaideoluwatobiloba1@gmail.com>
2023-07-20 10:32:38 +0100

    Added Anime

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ gsap.fromTo('.home-text h1', {opacity: 0, scale: 0, rotation: 720}, {duration: 1
 gsap.from('.about-part h3 span', { duration: 3, opacity: 0, delay: 1, stagger: .5 })
 
 
-
-
 const header = document.querySelector('header');
 window.addEventListener ('scroll', function() {
     header.classList.toggle ('sticky', window.scrollY > 100)
@@ -24,6 +22,22 @@ window.onscroll = () => {
     navbar.classList.remove('open');
 };
 
+window.addEventListener('scroll',function(){
+    var anime = document.querySelectorAll('.animeX');
 
+    for(var s = 0; s < anime.length; s++){
+        var windowheight = window.innerHeight;
+        var animetop = anime[s].getBoundingClientRect().top;
+        var animepoint = 150;
+
+        if(animetop < windowheight - animepoint){
+            anime[s].classList.add('active');
+        }
+        else{
+            anime[s].classList.remove('active');
+        }
+
+    }
+})
  
 
